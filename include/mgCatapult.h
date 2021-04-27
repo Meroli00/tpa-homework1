b15da398f05ae86a5eb1ade590af1fc03206a12c
Giacomo Mutti <83127466+MuttiGiacomo@users.noreply.github.com>
2021-04-27 18:34:59 +0200

    updated functions #2

--- include/mgCatapult.h
+++ include/mgCatapult.h
@@ -3,12 +3,31 @@
 
 #include <iostream>
 #include <string>
- 
+
 struct mgCatapult
 {
-    float arm,alfa,hbox,hbase,rad;
-};
+    float arm;
+    float alfa;
+    float hbox;
+    float hbase;
+    float rad;
 
+    mgCatapult(/*float _arm, float _alfa, float _hbox, float _hbase, float _rad*/){
+        arm = 0;
+        alfa = 0;
+        hbox = 0;
+        hbase = 0;
+        rad = 0;
+    }
+
+    void print()const{
+        fprintf(stdout,"Braccio: %d\nAlfa: %d\nAlt. cassone: %d\n, Alt. base: %d\nRaggio: %d",arm, alfa,hbox,hbase,rad);
+    }
+        
+};
+std::string mg_catSVG(mgCatapult* catapulta);    //costruisce la stringa
+int mg_check(mgCatapult* catapulta); 
 void mg_init_cat(mgCatapult* catapulta);
-void mg_check(mgCatapult* catapulta);
+
+
 #endif
